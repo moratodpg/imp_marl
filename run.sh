dd6842516d4c4d3c29c683bd1dcfe9e97320e86f
Pascal <pjr.leroy@gmail.com>
2022-08-01 13:59:13 +0200

    update cluster script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 #source env/bin/activate
-python main.py --config=$1 --env-config=struct with use_tensorboard=True
+python main.py --config=$1 --env-config=struct with env_args.components=$2 use_tensorboard=True name=$1_n_comp_$2
 #deactivate
