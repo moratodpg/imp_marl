5a745fa4a0f95f8452abdd77d522aca303953c02
Pablo G. Morato <pgmorato@uliege.be>
2022-09-09 16:46:37 +0200

    Update run.sh
    modified n_comp argument

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 #source env/bin/activate
-python main.py --config=$1 --env-config=struct with env_args.components=$2 env_args.k_comp=$3 use_tensorboard=True name=$1_comp_$2_$3_run$4
+python main.py --config=$1 --env-config=struct with env_args.n_comp=$2 env_args.k_comp=$3 use_tensorboard=True name=$1_comp_$2_$3_run$4
 #deactivate
