1df6549eb3aa96a9396557c494d586a230d8a056
Pablo G. Morato <pgmorato@uliege.be>
2022-09-27 17:20:27 +0200

    first implementation of ddmac

--- pymarl/learners/__init__.py
+++ pymarl/learners/__init__.py
@@ -3,6 +3,7 @@ from .qv_learner import QVLearner
 from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .comaIS_learner import COMAISLearner
+from .ddmac_learner import DDMACLearner
 from .maven_learner import MavenLearner
 REGISTRY = {}
 
@@ -11,3 +12,4 @@ REGISTRY["qv_learner"] = QVLearner
 REGISTRY["maxqv_learner"]= MaxQVLearner
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["comaIS_learner"] = COMAISLearner
+REGISTRY["ddmac_learner"] = DDMACLearner
