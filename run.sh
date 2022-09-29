0914d63f7f2f084695df470c2cf1a6a1df29d3a3
Pascal Leroy <pjr.leroy@gmail.com>
2022-09-29 18:55:00 +0200

    new config + new check

--- run.sh
+++ run.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 set -x
-source env/bin/activate
-python main.py
---config=$1
---env-config=struct
-with env_args.n_comp=$2
-env_args.discount_reward=$3
-env_args.k_comp=$4
-env_args.state_obs=$5
-env_args.state_d_rate=$6
-env_args.state_alphas=$7
-env_args.obs_d_rate=$8
-env_args.obs_multiple=$9
-env_args.obs_all_d_rate=$10
-env_args.obs_alphas=$11
-env_args.env_correlation=$12
-env_args.campaign_cost=$13
-use_tensorboard=$14
-name=$1_n_comp_$2_$3_run$4
 
+#
+# Warning: $3 and $4 are for discount_reward and k_comp but currently not used.
+source env/bin/activate
+python main.py --config=$1 --env-config=struct with env_args.n_comp=$2 env_args.state_obs=$5 env_args.state_d_rate=$6 env_args.state_alphas=$7 env_args.obs_d_rate=$8 env_args.obs_multiple=$9 env_args.obs_all_d_rate=${10} env_args.obs_alphas=${11} env_args.env_correlation=${12} env_args.campaign_cost=${13} use_tensorboard=True name=algo_$1_n_comp_$2_discount_reward_$3_k_comp_$4_state_obs_$5_state_d_rate_$6_state_alphas_$7_obs_d_rate_$8_obs_multiple_$9_obs_all_d_rate_${10}_obs_alphas_${11}_env_correlation_${12}_campaign_cost_${13}
 deactivate
---
+++ run_helper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+algo=$1
+n_comp=10
+discount_reward=100 # irrelevant for now
+k_comp=100 # irrelevant for now
+state_obs=True
+state_d_rate=False
+state_alphas=False
+obs_d_rate=False
+obs_multiple=False
+obs_all_d_rate=False
+obs_alphas=False
+env_correlation=False
+campaign_cost=False
+
+./run.sh $algo $n_comp $discount_reward $k_comp $state_obs $state_d_rate $state_alphas $obs_d_rate $obs_multiple $obs_all_d_rate $obs_alphas $env_correlation $campaign_cost
+
