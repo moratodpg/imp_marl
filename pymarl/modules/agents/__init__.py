70ddf0afb6905d4c480cb6480f8ed56d92423072
Pascal Leroy <pjr.leroy@gmail.com>
2022-10-28 17:55:01 +0200

    add a new class to be able to increase SARL network

--- pymarl/modules/agents/__init__.py
+++ pymarl/modules/agents/__init__.py
@@ -1,9 +1,10 @@
 
-
 REGISTRY = {}
 
 from .rnn_agent import RNNAgent
 from .rnn_v_agent import RNNVAgent
+from .rnn_agent_sarl import RNNAgentSARL
 
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["rnn_v"] = RNNVAgent
+REGISTRY["rnn_sarl"] = RNNAgentSARL
