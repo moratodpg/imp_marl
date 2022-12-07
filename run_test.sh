63dc42bd7914235abed94d6c5b7092b705e1041e
Pascal Leroy <pjr.leroy@gmail.com>
2022-12-07 11:17:15 +0100

    clean and test bash scripts

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 source env/bin/activate
-python main_run_test.py --config=qvmix --env-config=struct with test_nepisode=300 env_args.components=4 env_args.state_config=obs use_tensorboard=True name=test_qvmix__2022-08-04_14-51-52 checkpoint_path=/Users/pascalleroy/Documents/struct_marl/results/models/qvmix__2022-08-09_16-29-47
+alg=$1
+env=$2
+path="mypath"
+ntest= 300
+name=test_${alg}_${env}_aze123
+python main_run_test.py --config=${alg} --env-config=${env} with test_nepisode=${ntest} checkpoint_path=${path} name=${name}
 deactivate
 
