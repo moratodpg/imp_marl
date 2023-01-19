e1a9c7683ee3572995fc5ee37e6ad2289f1a687f
Pascal Leroy <pleroy@master.alan.priv>
2023-01-19 14:22:53 +0100

    save

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -x
-#source env/bin/activate
-#alg=dqn_sarl
-#env=struct_sarl_uc_5
-alg=iql_uc_50
-env=struct_uc_50
+source env/bin/activate
+alg=$1
+env=$2
 python main.py --config=${alg} --env-config=${env} with name=${alg}_${env}_$3
-#deactivate
+deactivate
