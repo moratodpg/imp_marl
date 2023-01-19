c65d7cff7440cfa623ed6be1968db9228e6a419c
Leroy Pascal <pjr.leroy@gmail.com>
2023-01-19 11:30:26 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -x
-#source env/bin/activate
-#alg=dqn_sarl
-#env=struct_sarl_uc_5
-alg=coma_uc_100
-env=struct_uc_100
+source env/bin/activate
+alg=$1
+env=$2
 python main.py --config=${alg} --env-config=${env} with name=${alg}_${env}_$3
-#deactivate
+deactivate
