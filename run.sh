e8ff388e3e8a13c616c39f7286596d239b98f623
Pascal Leroy <pleroy@master.alan.priv>
2023-03-31 18:03:02 +0200

    campaign cost tests

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
-source env/bin/activate
+conda activate marl_struct_gpu
 alg=$1
 env=$2
-python main.py --config=${alg} --env-config=${env} with name=${alg}_${env}_$3
+python main.py --config=${alg} --env-config=${env} with env_args.campaign_cost=$3 name=${alg}_${env}_cc_$3_$4 use_cuda=False test_nepisode=-1
 deactivate
