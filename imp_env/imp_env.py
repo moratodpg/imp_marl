7e94c5b253716b3b53e18e965f514ba758362518
pgmorato <pgmdo@dtu.dk>
2023-06-18 13:04:18 +0200

    interface documented

--- imp_env/imp_env.py
+++ imp_env/imp_env.py
@@ -1,14 +1,23 @@
 # Interface for IMP environment
 
 class ImpEnv(object):
+    """ Interface for creating IMP environments.
+
+    Methods:
+        reset
+        step
+    """
     def reset(self) -> dict:
         """
-        Returns damage probabilities in a dictionary.
+        Returns damage probabilities in a dictionary and resets the environment.
         """
         raise NotImplementedError
 
     def step(self, action: dict):
         """
-        Returns damage probabilities, reward, terminated, inspection_infos.
+        Returns damage probabilities, reward, terminated, inspection_infos and transitions the environment one time step.
+
+        Args:
+            action: A dictionary containing the actions assigned by each agent.
         """
         raise NotImplementedError
