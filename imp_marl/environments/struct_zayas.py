737bfd856b487dc953fc2a5d7085f91b14d2de99
Pablo Morato <moratosotelo@gmail.com>
2026-05-10 16:49:02 +0200

    set pf_sys instead of annual pf_sys

--- imp_marl/environments/struct_zayas.py
+++ imp_marl/environments/struct_zayas.py
@@ -284,14 +284,14 @@ class StructZayas(ImpEnv):
                     campaign_executed = True  # Campaign executed
         if self.n_comp < 2:  # single component setting
             PfSyS_ = PF_
-            PfSyS = PF
+            # PfSyS = PF
         else:
             PfSyS_ = self.pf_sys(PF_)
-            PfSyS = self.pf_sys(PF)
-        if PfSyS_ < PfSyS:
-            cost_system += PfSyS_ * (-100_000)
-        else:
-            cost_system += (PfSyS_ - PfSyS) * (-100_000)
+            # PfSyS = self.pf_sys(PF)
+        # if PfSyS_ < PfSyS:
+        cost_system += PfSyS_ * (-100_000)
+        # else:
+        #     cost_system += (PfSyS_ - PfSyS) * (-100_000)
         if campaign_executed:
             cost_system += -5
         return cost_system
